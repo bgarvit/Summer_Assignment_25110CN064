02aa4b3187fc8e1348287a56eb22f6695430186c
Harsh <2bgarvit@gmail.com>
2026-06-05 14:15:32 +0530

    Day01 Assinment Done

--- Day01/q03.cpp
+++ Day01/q03.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-    int n,i, fac = 1;
+    int n,i;
+    long long fac = 1;
     cout << "Enter the number to find its factorial :";
     cin >> n;
     for (i = 1; i <= n; i++)
