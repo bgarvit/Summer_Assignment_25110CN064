ec64c1e77c906235130b08d13dcebd012ebf9da4
Harsh <2bgarvit@gmail.com>
2026-06-12 17:20:34 +0530

    Day16 Assignment Done

--- Day16/Q63by 2 arrays.cpp
+++ Day16/Q63by 2 arrays.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
     int n,sum,nb=0,h;
-    long long b[10000000]={},a[10000000]={};
+    long long b[100000]={},a[100000]={};
     cout << "Define the elements of the array:"<<endl;
     cin >> n;
     cout << "Enter the elements of the array:"<<endl;
